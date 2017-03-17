536aa7ce08c54d84b77e0af9c72d927ae27cc4e1
nkshaneeb <shaneeb2015@gmail.com>
2017-03-17 08:53:35 +0530

    text and image provided after first intro button clicked

--- app/src/main/java/com/example/android/tourguide/IntroActivity.java
+++ app/src/main/java/com/example/android/tourguide/IntroActivity.java
@@ -2,6 +2,8 @@ package com.example.android.tourguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        Button firstIntroButton = (Button) findViewById(R.id.firstIntroButton_id);
+        firstIntroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
