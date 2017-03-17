4243a39d12bd5fde3b413cc8ea563db1bd0fc568
nkshaneeb <shaneeb2015@gmail.com>
2017-03-17 09:51:33 +0530

    content Adapter is created

---
+++ app/src/main/java/com/example/android/tourguide/ContentAdapter.java
@@ -0,0 +1,46 @@
+package com.example.android.tourguide;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by SHANEEB on 17-Mar-17.
+ */
+
+public class ContentAdapter extends ArrayAdapter<ContentClass> {
+    public ContentAdapter(Activity context, ArrayList<ContentClass> contents) {
+        super(context, 0, contents);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        ContentClass currentContent = getItem(position);
+
+        TextView textData = (TextView) listItemView.findViewById(R.id.textId);
+        ImageView imageData = (ImageView) listItemView.findViewById(R.id.imageId);
+
+        if (currentContent.hasImage()) {
+            imageData.setImageResource(currentContent.getmImageData());
+            imageData.setVisibility(View.VISIBLE);
+        } else {
+            imageData.setVisibility(View.GONE);
+        }
+
+
+        return listItemView;
+    }
+}
