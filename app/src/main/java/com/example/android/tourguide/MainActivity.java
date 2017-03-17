7d4c499258a6263a9fc657059a376e7491835925
nkshaneeb <shaneeb2015@gmail.com>
2017-03-17 08:32:39 +0530

    onclicklistener on introText and Intro Intent created

--- app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -21,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(introIntent);
             }
         });
+
+        TextView attractionTextView = (TextView) findViewById(R.id.attraction_id);
+        attractionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent attractionIntent =
+            }
+        });
     }
 }
