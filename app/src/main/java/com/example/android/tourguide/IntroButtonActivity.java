116a409872415ac123be55641ace99386c3b6393
nkshaneeb <shaneeb2015@gmail.com>
2017-03-17 09:30:00 +0530

    arraylist is created

--- app/src/main/java/com/example/android/tourguide/IntroButtonActivity.java
+++ app/src/main/java/com/example/android/tourguide/IntroButtonActivity.java
@@ -3,11 +3,16 @@ package com.example.android.tourguide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class IntroButtonActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_item);
+
+        final ArrayList <ContentClass> datas = new ArrayList<ContentClass>();
+        datas.add(new ContentClass("Testing testing testing", R.drawable.beachh));
     }
 }
